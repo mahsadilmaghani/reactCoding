2bc2778cd6c0d10c9e5d87e830a5bef421077905
Mahsa <mahsa@Admins-MacBook-Pro-7.local>
2020-05-13 09:12:47 +0100

    More coding practice - rendering tags

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        imageUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     styles = {
@@ -14,11 +15,23 @@ class Counter extends Component {
         return (
             <div>
                 <img src={this.state.imageUrl} alt=""/>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className={ this.getBadgeClasses() }>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <p>{this.state.tags.length === 0 && "There are no tags!"}</p>
+                {this.renderTags()}   
             </div>
         );
     }
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>Please create a new tag!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? "Zero" : count;
