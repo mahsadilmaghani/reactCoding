a3ea783878038b2b565321f1d49ef9fa0ed3a99d
Mahsa Dilmaghani <mahsa.dilmaghani@independent.co.uk>
2020-07-24 18:17:00 +0100

    Adding the NavBar

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import NavBar from './components/navBar';
 import './App.css';
 
-function App() {
-  return (
-    <main className='container'>
-      <Switch>
-        <Route path='/movies' component={Movies}></Route>
-        <Route path='/customers' component={Customers}></Route>
-        <Route path='/rentals' component={Rentals}></Route>
-        <Route path='/not-found' component={NotFound}></Route>
-        <Redirect from='/' exact to='/movies' />
-        <Redirect to='/not-found' />
-      </Switch>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path='/movies' component={Movies} />
+            <Route path='/customers' component={Customers} />
+            <Route path='/rentals' component={Rentals} />
+            <Route path='/not-found' component={NotFound} />
+            <Redirect from='/' exact to='/movies' />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
